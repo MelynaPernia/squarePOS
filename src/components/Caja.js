3de5e4b90cd65fda6214138ee455217498da2f0f
valeriavalles <valeria.sv@outlook.es>
2018-03-27 15:09:02 -0500

    actualizando header

--- src/components/Caja.js
+++ src/components/Caja.js
@@ -3,7 +3,7 @@ import React from 'react'
 
 
 const Caja = () => (
-    <div>
+    <div className="cont-padre">
         <h1>Cuadre de cajas</h1>
         <h2>Ventas del Día </h2>
         <hr/>
@@ -13,7 +13,17 @@ const Caja = () => (
         </div>
         <div className="total">
             <h3>Monto Total</h3>
-        </div>        
+            <div className="cont-inicial"></div>
+        </div>   
+        <div className="Pagos">
+            <h5>Pago Efectivo: {}</h5>
+            <h5>Pago Tarjeta: {}</h5>
+            <h5>Saldo Apertura: {}</h5>
+            <h5>Saldo Final: {}</h5>           
+        </div> 
+        <div className="cont-boton">
+        <button type="button" class="btn btn-primary">Cuadrar Caja</button>
+        </div>       
     </div>    
 )
 export default Caja
