8ebabaaa75992427d95437096adc92a89ceb186a
MelynaPernia <mebipeos@outlook.com>
2018-03-27 10:49:30 -0500

    Modificando la url de las imagenes

--- src/components/ItemProducts.js
+++ src/components/ItemProducts.js
@@ -1,11 +1,9 @@
-import React from 'react';
-const ItemProducts = ({ products }) => {
-  return (
+import React from 'react'
+const ItemProducts = ({products}) => (
     <div className="col-12 col-sm-3">
       <img className="img-fluid" src={products.photo} />
       <p>{products.name}</p>
       <p>{products.price}</p>
-    </div>
-  )
-}
+    </div>  
+)
 export default ItemProducts
