9329b902017f2c5f85c4ac02b4eb65ae52b76285
valeriavalles <valeria.sv@outlook.es>
2018-03-28 15:49:39 -0500

    actulizando los cambios

--- src/components/Products.js
+++ src/components/Products.js
@@ -1,11 +1,22 @@
 import React from 'react'
 import ItemProducts from './ItemProducts'
 import SaleProducts from './SaleProducts'
-const Products = ({ products, sale, addProduct, deleteProduct }) => (
+const Products = ({ products, sale, addProduct, deleteProduct }) => {
+  let total = 0;
+  for (let i = 0; i < sale.length; i++) {
+    for (let j = 0; j < products.length; j++) {
+      if (sale[i].id === products[j].id) {
+        total+=sale[i].cant*products[j].price
+      }
+
+    }
+  }
+return(
   <div>
     
     <ul className=" bg-light fixed-bottom mb-0  " title="cobrar">
     <button type="button" class="btn  boton">COBRAR</button>
+    <p>s/.{total}</p>
       {sale.map((element, index) =>
         <SaleProducts className="" key={index} id={element.id} products={products} cant={element.cant} />
       )}
@@ -21,4 +32,5 @@ const Products = ({ products, sale, addProduct, deleteProduct }) => (
 
   </div>
 )
+}
 export default Products
--- src/components/SaleProducts.js
+++ src/components/SaleProducts.js
@@ -5,11 +5,11 @@ const SaleProducts = ({ id, cant, products }) => {
   const pr = products.find(product => product.id === id);
   return (
     <div>
-  <span class="fi-check"></span>    
+  <span className="fi-check"></span>    
     <li className="lista">
-      <p>{` ${pr.name}  cant:${cant}`}</p>
+    <p>{ `${pr.name}  -  x${cant}   -  ${pr.coin}${pr.price * cant}`} </p>
     </li>
-    </div>
+   </div>
     )
 }
 export default SaleProducts
