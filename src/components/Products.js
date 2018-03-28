ae949421a2cd80fd7428275598af2e399ddb3ebc
MelynaPernia <mebipeos@outlook.com>
2018-03-28 10:23:04 -0500

    Añadiendo la lista de la venta de productos

--- src/components/Products.js
+++ src/components/Products.js
@@ -3,19 +3,19 @@ import ItemProducts from './ItemProducts'
 import SaleProducts from './SaleProducts'
 const Products = ({ products, sale, addProduct, deleteProduct }) => (
   <div>
-    <ul>
+    <ul className="bg-light fixed-bottom mb-0">
       {sale.map((element, index) =>
         <SaleProducts key={index} id={element.id} products={products} cant={element.cant} />
       )}
-    </ul>    
+    </ul>
 
-      <div className="row m-0 p-0 w-100">
-      
-        {products.map(element =>
-          <ItemProducts key={element.id} products={element} addProduct={addProduct} deleteProduct={deleteProduct}/>
-        )}
-      </div>
-    
+    <div className="row m-0 p-0 w-100">
+
+      {products.map(element =>
+        <ItemProducts key={element.id} products={element} addProduct={addProduct} deleteProduct={deleteProduct} />
+      )}
+    </div>
+
   </div>
 )
 export default Products
