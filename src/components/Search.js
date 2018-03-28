b936417cf85e3ddfd1b99d43fb112e5efdbc5407
MelynaPernia <mebipeos@outlook.com>
2018-03-28 09:42:49 -0500

    Modificando el componente Search

--- src/components/Search.js
+++ src/components/Search.js
@@ -5,7 +5,7 @@ const Search = () => (
     <br/>
     <div className="form-group d-flex justify-content-center">
       <div className="col-7">
-        <input type="text" className="form-control" placeholder="City" />
+        <input type="text" className="form-control" placeholder="Producto..." />
         <br/>
       </div>
     </div>
